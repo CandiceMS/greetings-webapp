692a07e9135e17a0f1d134d24ec2d99732e8c91f
Candice <candice.m.sprout@gmail.com>
2018-08-31 11:26:22 +0200

    Initial commit

---
+++ greetings.js
@@ -0,0 +1,91 @@
+module.exports = function () {
+  // function (storedMap)
+  let greetCount = 0;
+  let map = {};
+
+  // function storeMap(){
+  //   if (storedMap) {
+  //     map = storedMap;
+  //   }
+  // }
+
+  function assignName(checkedLanguage, nameInput) {
+// storeMap();
+  let nameLower = nameInput.toLowerCase();
+  let name = nameLower.charAt(0).toUpperCase() + nameLower.slice(1);
+
+    if (name !== '' && checkedLanguage) {
+      if (map[name] === undefined){
+        map[name] = 0;
+      }
+      map[name] += 1;
+     }
+  }
+
+  function returnMap() {
+    return Object.keys(map);
+  }
+
+  function counter() {
+    greetCount = Object.keys(map).length;
+    return greetCount;
+    }
+
+  function returnUser(username) {
+    return map[username];
+  }
+
+  function alert(nameInput, checkedLanguage) {
+    if (!nameInput && !checkedLanguage) {
+      return "Please enter your name and select a language!"
+    }
+    else if (nameInput === "") {
+        return "Please enter your name!"
+//change return string response. This one is for testing purposes.
+    }
+    else if (!checkedLanguage) {
+      return "Please select a language!"
+//change return string response. This one is for testing purposes.
+    }
+    else {
+      return "";
+    }
+    }
+
+  function greetPerson(checkedLanguage, nameInput) {
+    let nameLower = nameInput.toLowerCase();
+    let name = nameLower.charAt(0).toUpperCase() + nameLower.slice(1);
+    let greet = ''
+      if (checkedLanguage === "english") {
+        greet = "Hello ";
+      }
+      if (checkedLanguage === "sesotho") {
+        greet = "Dumela ";
+      }
+      if (checkedLanguage === "french") {
+        greet = "Bonjour ";
+      }
+      if (checkedLanguage && nameInput) {
+        return greet + name;
+      }
+      else {
+        return "";
+      }
+  }
+
+  function resetCount() {
+    map = {};
+    greetCount = 0;
+  }
+
+return {
+  // storeMap,
+  assignName,
+  returnMap,
+  greetPerson,
+  counter,
+  alert,
+  resetCount,
+  returnUser
+}
+}
---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+let greetings = require('./greetings');
+let greet = greetings();
+
+let PORT = process.env.PORT || 3500;
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+app.listen(PORT, function() {
+  console.log('App starting on port', PORT);
+});
+
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+// app.get('/greetings', function (req, res) {
+//     req.flash('info', 'Flash Message Added');
+//     res.redirect('/');
+// });
+
+app.post('/greetings', function(req, res) {
+  let language = req.body.language;
+  let name = req.body.inputName;
+    greet.assignName(language, name);
+  res.render('home', {
+    alert: greet.alert(name, language),
+    greetPerson: greet.greetPerson(language, name),
+    count: greet.counter()
+  });
+});
+
+app.get('/greeted', function(req, res) {
+  res.render('greeted', {
+    userCount: greet.returnMap()
+  });
+});
+
+app.get('/greeted/:user', function(req, res) {
+  res.render('greetedUser', {
+    greetNumber: greet.returnUser(user)
+  });
+});
+
+app.post('/clear', function(req, res) {
+   greet.resetCount();
+  res.redirect('/');
+});
+
+// app.get('/actions/:type', function(req, res) {
+//   res.render('filter', {
+//     actionFilter: setBill.filterActions(req.params.type),
+//     totalForAction: setBill.actionTotal(req.params.type)
+//   });
+// });
